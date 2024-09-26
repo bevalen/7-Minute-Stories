de7c88bc7af2c0699d18beaf7264fd5392f15e63
Ben_Valentin <bjoel.valentin@gmail.com>
2024-09-26 14:56:51 -0400

    Updated mobile styling

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,13 @@ function Home() {
         with AI
       </span>
       <p className="mt-4 text-center text-base md:text-lg font-medium text-gray-700 mx-4">
-        &quot;...always be prepared to give an answer to everyone who asks you
-        <br />
-        to give the reason for the hope that you have.&quot; - 1 Peter 3:15
+        <span className="hidden md:inline">&quot;...always be prepared to give an answer to everyone who asks you</span>
+        <span className="hidden md:inline"><br /></span>
+        <span className="hidden md:inline">to give the reason for the hope that you have.&quot; - 1 Peter 3:15</span>
+        
+        <span className="md:hidden">&quot;...always be prepared to give an answer to everyone who asks you to give the reason</span>
+        <span className="md:hidden"><br /></span>
+        <span className="md:hidden">for the hope that you have.&quot; - 1 Peter 3:15</span>
       </p>
       <Link href="/testimony">
         <Button variant="default" className="mt-6">
